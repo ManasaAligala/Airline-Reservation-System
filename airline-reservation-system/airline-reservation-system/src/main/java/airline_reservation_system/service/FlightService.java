b452ce03d58c3fbb25f5cb6adb41d56e8ebb83a7
Manasa <manasaaligala502@gmail.com>
2026-08-27 23:17:24 +0530

    FlightService.java

---
+++ airline-reservation-system/airline-reservation-system/src/main/java/airline_reservation_system/service/FlightService.java
@@ -0,0 +1,18 @@
+package airline_reservation_system.service;
+
+import airline_reservation_system.entity.Flight;
+
+import java.util.List;
+
+public interface FlightService {
+
+    Flight createFlight(Flight flight);
+
+    List<Flight> getAllFlights();
+
+    Flight getFlightById(Long id);
+
+    Flight updateFlight(Long id, Flight flight);
+
+    void deleteFlight(Long id);
+}
