256bc3abd7314c7d82f48ecf0364c970fab05a5f
Manasa <manasaaligala502@gmail.com>
2026-09-04 00:18:33 +0530

    added modified App.jsx

--- airline-frontend/src/App.jsx
+++ airline-frontend/src/App.jsx
@@ -1,15 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import FlightSearch from "./pages/FlightSearch";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<FlightSearch />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route path="/flights/search" element={<FlightSearch />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
