04115d3a4500d185de1d7fc9719a626f05081e17
Manasa <manasaaligala502@gmail.com>
2026-09-03 23:32:21 +0530

    modified FlightScheduleRepository.java

--- airline-reservation-system/airline-reservation-system/src/main/java/airline_reservation_system/repository/FlightScheduleRepository.java
+++ airline-reservation-system/airline-reservation-system/src/main/java/airline_reservation_system/repository/FlightScheduleRepository.java
@@ -4,6 +4,16 @@ import airline_reservation_system.entity.FlightSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface FlightScheduleRepository extends JpaRepository<FlightSchedule, Long> {
+
+    List<FlightSchedule> findByFlight_DepartureAirport_CodeAndFlight_ArrivalAirport_CodeAndDepartureTimeGreaterThanEqualAndDepartureTimeLessThan(
+            String from,
+            String to,
+            LocalDateTime start,
+            LocalDateTime end
+    );
 }
