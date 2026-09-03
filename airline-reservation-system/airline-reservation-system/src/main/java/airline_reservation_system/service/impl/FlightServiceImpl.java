2aa86af771df06ff32f4bb9abc743a0f8917b6ce
Manasa <manasaaligala502@gmail.com>
2026-09-03 23:30:35 +0530

    added FlightServiceimpl.java

--- airline-reservation-system/airline-reservation-system/src/main/java/airline_reservation_system/service/impl/FlightServiceImpl.java
+++ airline-reservation-system/airline-reservation-system/src/main/java/airline_reservation_system/service/impl/FlightServiceImpl.java
@@ -1,21 +1,55 @@
 package airline_reservation_system.service.impl;
 
 import airline_reservation_system.entity.Flight;
+import airline_reservation_system.entity.FlightSchedule;
 import airline_reservation_system.repository.FlightRepository;
+import airline_reservation_system.repository.FlightScheduleRepository;
 import airline_reservation_system.service.FlightService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class FlightServiceImpl implements FlightService {
 
     private final FlightRepository flightRepository;
+    private final FlightScheduleRepository flightScheduleRepository;
 
-    public FlightServiceImpl(FlightRepository flightRepository) {
+    public FlightServiceImpl(
+            FlightRepository flightRepository,
+            FlightScheduleRepository flightScheduleRepository) {
+
         this.flightRepository = flightRepository;
+        this.flightScheduleRepository = flightScheduleRepository;
     }
 
+    // Search Flights
+    @Override
+    public List<Flight> searchFlights(String from, String to, String date) {
+
+        LocalDate searchDate = LocalDate.parse(date);
+
+        LocalDateTime startOfDay = searchDate.atStartOfDay();
+        LocalDateTime startOfNextDay = searchDate.plusDays(1).atStartOfDay();
+
+        List<FlightSchedule> schedules =
+                flightScheduleRepository
+                        .findByFlight_DepartureAirport_CodeAndFlight_ArrivalAirport_CodeAndDepartureTimeGreaterThanEqualAndDepartureTimeLessThan(
+                                from,
+                                to,
+                                startOfDay,
+                                startOfNextDay
+                        );
+
+        return schedules.stream()
+                .map(FlightSchedule::getFlight)
+                .distinct()
+                .toList();
+    }
+
+    // Create Flight
     @Override
     public Flight createFlight(Flight flight) {
 
@@ -25,6 +59,7 @@ public class FlightServiceImpl implements FlightService {
 
         if (flight.getDepartureAirport().getId()
                 .equals(flight.getArrivalAirport().getId())) {
+
             throw new RuntimeException(
                     "Departure and arrival airports cannot be the same"
             );
@@ -33,11 +68,13 @@ public class FlightServiceImpl implements FlightService {
         return flightRepository.save(flight);
     }
 
+    // Get All Flights
     @Override
     public List<Flight> getAllFlights() {
         return flightRepository.findAll();
     }
 
+    // Get Flight By ID
     @Override
     public Flight getFlightById(Long id) {
 
@@ -47,6 +84,7 @@ public class FlightServiceImpl implements FlightService {
                 );
     }
 
+    // Update Flight
     @Override
     public Flight updateFlight(Long id, Flight flight) {
 
@@ -66,6 +104,7 @@ public class FlightServiceImpl implements FlightService {
         return flightRepository.save(existingFlight);
     }
 
+    // Delete Flight
     @Override
     public void deleteFlight(Long id) {
 
