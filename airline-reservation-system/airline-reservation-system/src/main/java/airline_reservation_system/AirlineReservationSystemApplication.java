0cf30a80959132884ec30fd3e30cd4a844673b30
Manasa <manasaaligala502@gmail.com>
2026-09-06 23:51:53 +0530

    modified main file

--- airline-reservation-system/airline-reservation-system/src/main/java/airline_reservation_system/AirlineReservationSystemApplication.java
+++ airline-reservation-system/airline-reservation-system/src/main/java/airline_reservation_system/AirlineReservationSystemApplication.java
@@ -8,6 +8,7 @@ public class AirlineReservationSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AirlineReservationSystemApplication.class, args);
+		System.out.println("Backend working properly");
 	}
 
 }
