061670c6f9fc59034e1c90c2a678a6d3605e8e7c
Manasa <manasaaligala502@gmail.com>
2026-09-08 21:53:15 +0530

    modified SeatServiceImpl.java

--- airline-reservation-system/airline-reservation-system/src/main/java/airline_reservation_system/service/impl/SeatServiceImpl.java
+++ airline-reservation-system/airline-reservation-system/src/main/java/airline_reservation_system/service/impl/SeatServiceImpl.java
@@ -4,6 +4,7 @@ import airline_reservation_system.entity.Seat;
 import airline_reservation_system.entity.SeatStatus;
 import airline_reservation_system.repository.SeatRepository;
 import airline_reservation_system.service.SeatService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,10 +12,25 @@ import java.util.List;
 @Service
 public class SeatServiceImpl implements SeatService {
 
-    private final SeatRepository seatRepository;
+    @Autowired
+    private SeatRepository seatRepository;
 
-    public SeatServiceImpl(SeatRepository seatRepository) {
-        this.seatRepository = seatRepository;
+    @Override
+    public Seat selectSeat(Long seatId) {
+
+        Seat seat = seatRepository.findById(seatId)
+                .orElseThrow(() -> new RuntimeException("Seat not found"));
+
+        // Task 5: Prevent duplicate booking
+        if (seat.getStatus() == SeatStatus.BOOKED) {
+            throw new RuntimeException(
+                    "Seat " + seat.getSeatNumber() + " is already booked"
+            );
+        }
+
+        seat.setStatus(SeatStatus.BOOKED);
+
+        return seatRepository.save(seat);
     }
 
     @Override
