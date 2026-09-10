46c83f103ac47b7b56842d3fbe55e7f09aff368c
Manasa <manasaaligala502@gmail.com>
2026-09-10 23:25:25 +0530

    Modified

--- airline-reservation-system/airline-reservation-system/src/main/java/airline_reservation_system/controller/BookingController.java
+++ airline-reservation-system/airline-reservation-system/src/main/java/airline_reservation_system/controller/BookingController.java
@@ -2,7 +2,6 @@ package airline_reservation_system.controller;
 
 import airline_reservation_system.entity.Booking;
 import airline_reservation_system.service.BookingService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,37 +21,29 @@ public class BookingController {
     public ResponseEntity<Booking> createBooking(
             @RequestBody Booking booking) {
 
-        Booking savedBooking = bookingService.createBooking(booking);
+        Booking createdBooking = bookingService.createBooking(booking);
 
-        return new ResponseEntity<>(
-                savedBooking,
-                HttpStatus.CREATED
-        );
+        return ResponseEntity.ok(createdBooking);
     }
 
-    @GetMapping
-    public ResponseEntity<List<Booking>> getAllBookings() {
-
-        return ResponseEntity.ok(
-                bookingService.getAllBookings()
-        );
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Booking> getBookingById(
-            @PathVariable Long id) {
-
-        return ResponseEntity.ok(
-                bookingService.getBookingById(id)
-        );
-    }
-
-    @GetMapping("/booking-id/{bookingId}")
-    public ResponseEntity<Booking> getBookingByBookingId(
+    @PutMapping("/confirm/{bookingId}")
+    public ResponseEntity<String> confirmBooking(
             @PathVariable String bookingId) {
 
+        bookingService.confirmBooking(bookingId);
+
         return ResponseEntity.ok(
-                bookingService.getBookingByBookingId(bookingId)
+                "Booking " + bookingId + " confirmed successfully"
         );
+    }
+
+    // Task 7 - Booking History
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<Booking>> getBookingsByUserId(
+            @PathVariable Long userId) {
+
+        List<Booking> bookings = bookingService.getBookingsByUserId(userId);
+
+        return ResponseEntity.ok(bookings);
     }
 }
