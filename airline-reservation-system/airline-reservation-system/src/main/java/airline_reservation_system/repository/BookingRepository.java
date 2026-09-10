0428ef315da3a2017ac055ae3c23bbb850d2cb0a
Manasa <manasaaligala502@gmail.com>
2026-09-10 23:24:41 +0530

    modified

--- airline-reservation-system/airline-reservation-system/src/main/java/airline_reservation_system/repository/BookingRepository.java
+++ airline-reservation-system/airline-reservation-system/src/main/java/airline_reservation_system/repository/BookingRepository.java
@@ -2,7 +2,7 @@ package airline_reservation_system.repository;
 
 import airline_reservation_system.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
@@ -10,5 +10,8 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     Optional<Booking> findByBookingId(String bookingId);
 
     boolean existsByBookingId(String bookingId);
+
+    List<Booking> findByUserId(Long userId);
+   
 
 }
