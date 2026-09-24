5699fe70fb68a8c1f6e002831ec6137a4774e917
Manasa <manasaaligala502@gmail.com>
2026-09-24 23:22:24 +0530

    Added PaymentRepo.java

---
+++ airline-reservation-system/airline-reservation-system/src/main/java/airline_reservation_system/repository/PaymentRepository.java
@@ -0,0 +1,11 @@
+package airline_reservation_system.repository;
+
+import airline_reservation_system.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    Optional<Payment> findByPaymentId(String paymentId);
+}
